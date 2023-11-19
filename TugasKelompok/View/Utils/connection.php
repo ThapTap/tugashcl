629dcd0a0ce47eeee61fe0b502e560e90c39a9fb
mandaania <amandanathania189@gmail.com>
2023-11-19 13:14:18 +0700

    benerin connection.php

--- TugasKelompok/View/Utils/connection.php
+++ TugasKelompok/View/Utils/connection.php
@@ -1,10 +1,15 @@
 <?php
-    require "./database.php";
-    $db = mysqli_connect(
-        $config['server'],
-        $config["username"],
-        $config["password"],
-        $config["database"]
-    )
+    // Directly set the database configuration here
+    $server = '127.0.0.1';
+    $username = 'root';
+    $password = '';
+    $database = 'medilogdb';
 
+    // Attempt to establish a connection
+    $conn = mysqli_connect($server, $username, $password, $database);
+
+    // Check connection
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
 ?>
