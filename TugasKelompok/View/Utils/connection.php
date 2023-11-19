538f2bb1a2b3ef92285102f6d073153171d74c1a
mandaania <amandanathania189@gmail.com>
2023-11-19 13:26:53 +0700

    Change to $user

--- TugasKelompok/View/Utils/connection.php
+++ TugasKelompok/View/Utils/connection.php
@@ -1,12 +1,12 @@
 <?php
     // Directly set the database configuration here
     $server = '127.0.0.1';
-    $username = 'root';
-    $password = '';
+    $user = 'root';
+    $pass = '';
     $database = 'medilogdb';
 
     // Attempt to establish a connection
-    $conn = mysqli_connect($server, $username, $password, $database);
+    $conn = mysqli_connect($server, $user, $pass, $database);
 
     // Check connection
     if (!$conn) {
